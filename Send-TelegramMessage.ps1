659e1cad34f83a16e31c69b2c6ab76a894fd6091
Bruce Wayne <admin@bige0.com>
2023-06-04 23:10:51 +0800

    test

--- Send-TelegramMessage.ps1
+++ Send-TelegramMessage.ps1
@@ -10,8 +10,8 @@ if ($current_version -ge $latest_version) {
 
 function Send-TelegramMessage {
 	param($message)
-	$url = 'https://api.telegram.org/bot' + $TELEGRAM_BOT_APITOKEN + '/sendMessage'
-	$body = "chat_id=$TELEGRAM_BOT_CHATID&text=$message&parse_mode=MarkdownV2&disable_web_page_preview=true"
+	$url = 'https://api.telegram.org/bot' + $env:TELEGRAM_BOT_APITOKEN + '/sendMessage'
+	$body = "chat_id=$env:TELEGRAM_BOT_CHATID&text=$message&parse_mode=MarkdownV2&disable_web_page_preview=true"
 	Invoke-WebRequest $url -Method 'POST' -Body $body -ContentType "application/x-www-form-urlencoded; charset=utf-8"
 }
 
